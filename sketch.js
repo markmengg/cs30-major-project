a9480376e8f99a155ad1f0f747ddeea673584fea
markmengg <markmengg@gmail.com>
2024-11-21 12:15:24 -0600

    Made additions to the project description, needs to have, and nice to have list in the proposal aspect of this project.

--- sketch.js
+++ sketch.js
@@ -6,6 +6,21 @@
 // - describe what you did to take this project "above and beyond"
 
 
+//(9*5 gird)
+let grid = [];
+const COLS = 9;
+const ROWS = 5;
+let gameState = "beginning";
+
+
+let grid_default;
+
+
+
+function preload(){
+  
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
