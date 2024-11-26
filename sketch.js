c307781390175e48ec983aec49bb3c77cd229001
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-11-26 13:53:54 -0600

    Added the hovered versions of our start menu buttons

--- sketch.js
+++ sketch.js
@@ -20,13 +20,25 @@ let lawnend;
 let sm_background;
 
 let sm_adventure;
+let sm_adventure_hovered;
+
 let sm_minigame;
+let sm_minigame_hovered;
+
 let sm_puzzle;
+let sm_puzzle_hovered;
+
 let sm_survival;   
+let sm_survival_hovered;
 
 let sm_options;
+let sm_options_hovered;
+
 let sm_help;
+let sm_help_hovered;
+
 let sm_quit;
+let sm_quit_hovered;
 
 
 
@@ -65,6 +77,8 @@ function preload(){
   sm_options = loadImage("menus/startmenu/buttons/options.png");
   sm_help =loadImage("menus/startmenu/buttons/help.png");
   sm_quit =loadImage("menus/startmenu/buttons/quit.png");
+
+  // Start Menu Buttons Hovered
 
 
 
@@ -111,9 +125,11 @@ function mousexy(){
 }
 
 function startMenu() {
-  
   image(sm_background, 0, 0);
-
   image(sm_adventure, 391, 64); 
-
+  image(sm_minigame,397,160)//(13,18)
+  image(sm_puzzle,410,259)
+  image(sm_survival,417,326)
 }
+
+function 
