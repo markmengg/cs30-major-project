e7e393ceaf3692690fefa9c6d76b589f346b289a
sldyaAkali <156632646+sldyaAkali@users.noreply.github.com>
2024-11-28 18:03:50 -0600

    resize starmenu images

--- sketch.js
+++ sketch.js
@@ -12,184 +12,134 @@ let bg_house;
 let bg_road;
 let bg_topFence;
 let bg_bottomTile;
-
 let lawn;
 let lawnend;
 
 // Start Menu Images
 let sm_background;
-
-let sm_adventure;
 let sm_adventure_hovered;
-
-let sm_minigame;
 let sm_minigame_hovered;
-
-let sm_puzzle;
 let sm_puzzle_hovered;
-
-let sm_survival;   
 let sm_survival_hovered;
-
-let sm_options;
 let sm_options_hovered;
-
-let sm_help;
 let sm_help_hovered;
-
-let sm_quit;
 let sm_quit_hovered;
 
-
-
-
-
-
-
-//(9*5 gird)
+// Grid (9x5)
 let grid = [];
 const COLS = 9;
 const ROWS = 5;
 let gameState = "start";
 
 
-let grid_default;
-let sunGif;
 
-// Start Screen Buttons
-let adventureButton;
-let minigButton;
-let puzzleButton;
-let survivalButton;
-let optionsButton;
-let helpButton;
-let quitButton;
 
-function preload(){
-  // Background Images
-  bg_bottomTile = loadImage("bgImages/day/bottomtile.png");
+function preload() {
+  // Load images
   bg_house = loadImage("bgImages/day/house.png");
   bg_topFence = loadImage("bgImages/day/topFence.png");
-  bg_road= loadImage("bgImages/day/road.png");
+  bg_bottomTile = loadImage("bgImages/day/bottomtile.png");
   lawn = loadImage("bgImages/day/lawn.png");
   lawnend = loadImage("bgImages/day/lawnroadinbetween.png");
-  
-  // Start Menu
-  sm_background = loadImage("menus/startmenu/bg.png");
 
-  // Start Menu Buttons Hovered
+  sm_background = loadImage("menus/startmenu/bg.png");
   sm_adventure_hovered = loadImage("menus/startmenu/buttons/hovered/adventure.png");
   sm_minigame_hovered = loadImage("menus/startmenu/buttons/hovered/minig.png");
   sm_puzzle_hovered = loadImage("menus/startmenu/buttons/hovered/puzzle.png");
   sm_survival_hovered = loadImage("menus/startmenu/buttons/hovered/survival.png");
-  
   sm_options_hovered = loadImage("menus/startmenu/buttons/hovered/options.png");
-  sm_help_hovered =loadImage("menus/startmenu/buttons/hovered/help.png");
-  sm_quit_hovered =loadImage("menus/startmenu/buttons/hovered/quit.png");
-  
-
-
-  sunGif = loadImage("GIFs/sun.gif");
-
-
+  sm_help_hovered = loadImage("menus/startmenu/buttons/hovered/help.png");
+  sm_quit_hovered = loadImage("menus/startmenu/buttons/hovered/quit.png");
 }
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  imageResize(); // Resize all images once during setup
 }
 
 function draw() {
   background(220);
   if (gameState === "start") {
     startMenu();
-  }
-  else if (gameState === "adventureStart") {
+  } else if (gameState === "adventureStart") {
     displayBackground();
   }
-  //displayBackground();
-  displayMouseXY();//delete last
-  
+  displayMouseXY(); // For debugging
 }
 
-function displayBackground() {
-  
-  image(bg_house, 0, 0); // Start of pan
-  image(bg_topFence,bg_house.width,0);
-  image(bg_bottomTile,bg_house.width,bg_topFence.height+lawn.height);
-  image(lawnend,bg_house.width+lawn.width,0);
-  image(lawn, bg_house.width, bg_topFence.height);
+function imageResize() {
+  // Resize the background 
+  let scalexHome = height/3*4 / sm_background.width;
+  let scaleyHome = height / sm_background.height;
+  sm_background.resize((height / 3) * 4, height);
 
-  // Only for camera pan
-  // image(bg_road,bg_house.width+bg_topFence.width+lawnend.width, 0);
-
-}
-  
-
-
-function displayMouseXY(){
-  textSize(24);
-  fill("yellow");
-  text("X: " + mouseX + "  Y: " + mouseY, 10, 20);
+  sm_adventure_hovered.resize(sm_adventure_hovered.width * scalexHome, sm_adventure_hovered.height * scaleyHome);
+  sm_minigame_hovered.resize(sm_minigame_hovered.width * scalexHome, sm_minigame_hovered.height * scaleyHome);
+  sm_puzzle_hovered.resize(sm_puzzle_hovered.width * scalexHome, sm_puzzle_hovered.height * scaleyHome);
+  sm_survival_hovered.resize(sm_survival_hovered.width * scalexHome, sm_survival_hovered.height * scaleyHome);
+  sm_options_hovered.resize(sm_options_hovered.width * scalexHome, sm_options_hovered.height * scaleyHome);
+  sm_help_hovered.resize(sm_help_hovered.width * scalexHome, sm_help_hovered.height * scaleyHome);
+  sm_quit_hovered.resize(sm_quit_hovered.width * scalexHome, sm_quit_hovered.height * scaleyHome);
 }
 
 function startMenu() {
-  imageResize()
-  image(sm_background, (width-sm_background.width)/2, 0);
-  startMenuHovered();
-  
+  image(sm_background, (width - sm_background.width) / 2, 0); // Center the background
+  startMenuHovered(); // Check if mouse is hovering over buttons
 }
 
-function imageResize(){
-  sm_background.resize(height/3*4,height);
+function displayBackground() {
+  image(bg_house, 0, 0);
+  image(bg_topFence, bg_house.width, 0);
+  image(bg_bottomTile, bg_house.width, bg_topFence.height + lawn.height);
+  image(lawnend, bg_house.width + lawn.width, 0);
+  image(lawn, bg_house.width, bg_topFence.height);
 }
-
-
-
 
 function startMenuHovered() {
   // Adventure Button
-  if (mouseX >= 413 && mouseX <= 731 && mouseY > 94 && mouseY < 176) {
-    image(sm_adventure_hovered, 405, 64);
+  if (mouseX >= 946 && mouseX <= 1451 && mouseY > 98 && mouseY < 260) {
+    image(sm_adventure_hovered, 946,98);
   }
   // Mini Game Button
-  if (mouseX >= 407 && mouseX <= 711 && mouseY > 178 && mouseY < 263) {
-    image(sm_minigame_hovered, 405, 171);
-  } 
+  if (mouseX >= 945 && mouseX <= 1428 && mouseY > 275 && mouseY < 383) {
+    image(sm_minigame_hovered, 945, 267);
+  }
   // Puzzle Button
-  if (mouseX >= 413 && mouseX <= 687 && mouseY > 278 && mouseY < 346) {
-    image(sm_puzzle_hovered, 410, 257);
+  if (mouseX >= 959 && mouseX <= 1387 && mouseY > 400 && mouseY < 489) {
+    image(sm_puzzle_hovered, 953, 397);
   }
   // Survival Button
-  if (mouseX >= 420 && mouseX <= 675 && mouseY > 351 && mouseY < 409) {
-    image(sm_survival_hovered, 416, 325);
+  if (mouseX >= 967 && mouseX <= 1365 && mouseY > 511 && mouseY < 594) {
+    image(sm_survival_hovered, 960, 505);
   }
   // Options Button
-  if (mouseX >= 565 && mouseX <= 640 && mouseY > 473 && mouseY < 528) {
-    image(sm_options_hovered, 551, 427);
+  if (mouseX >= 1182 && mouseX <= 1307 && mouseY > 695 && mouseY < 821) {
+    image(sm_options_hovered, 1171, 661);
   }
   // Help Button
-  if (mouseX >= 650 && mouseX <= 698 && mouseY > 492 && mouseY < 550) {
-    image(sm_help_hovered, 637, 412);
+  if (mouseX >= 1328 && mouseX <= 1385 && mouseY > 724 && mouseY < 854) {
+    image(sm_help_hovered, 1305, 638);
   }
   // Quit Button
-  if (mouseX >= 705 && mouseX <= 770 && mouseY > 470 && mouseY < 550) {
-    image(sm_quit_hovered, 702, 441);
+  if (mouseX >= 1430 && mouseX <= 1517 && mouseY > 699 && mouseY < 841) {
+    image(sm_quit_hovered, 1405,684);
   }
 }
 
+function displayMouseXY() {
+  textSize(24);
+  fill("black");
+  text("X: " + mouseX + "  Y: " + mouseY, 10, 20);
+}
 
-function mouseReleased(){
-  if (gameState === "start"){
-    if (mouseX >= 413 && mouseX <= 731 && mouseY > 94 && mouseY < 176) {
+function mouseReleased() {
+  if (gameState === "start") {
+    if (mouseX >= 946 && mouseX <= 1451 && mouseY > 98 && mouseY < 260) {
       gameState = "adventureStart";
-    }
-    // Game modes part of needs to have
-    // if (mouseX >= 407 && mouseX <= 711 && mouseY > 178 && mouseY < 263){
-    //   gameState = "minigameStart";
-    if (mouseX >= 705 && mouseX <= 770 && mouseY > 470 && mouseY < 550) {
+    } else if (mouseX >= 1430 && mouseX <= 1517 && mouseY > 699 && mouseY < 841) {
       window.close();
     }
   }
 }
-
-
